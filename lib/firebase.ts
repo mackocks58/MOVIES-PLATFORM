779b2c63151ef810a1b532ad3c9be0542ac05c31
mackocks58 <mackocks58@gmail.com>
2026-03-20 10:33:01 +0300

    Hardcode Firebase credentials to fix Vercel build errors

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase, Database } from 'firebase/database';
 import { getStorage, FirebaseStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAIX3aQq3VEBq269Jdrk77CefNttqAR51s",
+  authDomain: "mozambique-newhope.firebaseapp.com",
+  projectId: "mozambique-newhope",
+  storageBucket: "mozambique-newhope.firebasestorage.app",
+  messagingSenderId: "133563964959",
+  appId: "1:133563964959:web:d3f183b721d540140f7f2a",
+  databaseURL: "https://mozambique-newhope-default-rtdb.firebaseio.com",
+  measurementId: "G-FY6CNVX6ZK",
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
